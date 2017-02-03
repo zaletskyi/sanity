14c1c75763aa281ecd4025d5c766d1aceb29bb3a
Bjørge Næss <bjoerge@gmail.com>
2017-02-03 14:32:48 +0100

    Minor fixes

--- packages/@sanity/form-builder/src/index.js
+++ packages/@sanity/form-builder/src/index.js
@@ -3,9 +3,9 @@ import * as FileInput from './inputs/File'
 import * as ImageInput from './inputs/Image'
 import * as ReferenceInput from './inputs/Reference'
 import * as SlugInput from './inputs/Slug'
-import SlateInput from './inputs/BlockEditor-slate'
 
 export {defaultInputs}
+export {default as SlateInput} from './inputs/BlockEditor-slate'
 
 export {default as defaultConfig} from './defaultConfig'
 export {default as createFormBuilder} from './createFormBuilder'
@@ -15,4 +15,3 @@ export {ReferenceInput}
 export {ImageInput}
 export {FileInput}
 export {SlugInput}
-export {SlateInput}
