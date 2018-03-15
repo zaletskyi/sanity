8e1b09a35c08b59d906cf7dbe0cfcd15037e7edc
Thomas Drevon <thomas@bengler.no>
2018-03-15 10:00:48 +0100

    [default-layout] Make sure tool state key isn't applied to the new state

--- packages/@sanity/default-layout/src/components/ToolSwitcher.js
+++ packages/@sanity/default-layout/src/components/ToolSwitcher.js
@@ -4,6 +4,7 @@ import {StateLink, withRouterHOC} from 'part:@sanity/base/router'
 import styles from './styles/ToolSwitcher.css'
 import PluginIcon from 'part:@sanity/base/plugin-icon'
 import Ink from 'react-ink'
+import {omit} from 'lodash'
 
 function ToolSwitcher(props) {
   const {tools, router, activeToolName, onSwitchTool} = props
@@ -17,10 +18,13 @@ function ToolSwitcher(props) {
 
           const ToolIcon = tool.icon || PluginIcon
 
-          const state = Object.assign({}, router.state, {tool: tool.name})
+          const nextState = {
+            ...omit(router.state, router.state.tool), // clear current tool's state key
+            tool: tool.name
+          }
           return (
             <li key={tool.name} className={itemClass}>
-              <StateLink className={styles.toolLink} state={state} onClick={onSwitchTool}>
+              <StateLink className={styles.toolLink} state={nextState} onClick={onSwitchTool}>
                 <div className={styles.iconContainer}>
                   <ToolIcon />
                 </div>
